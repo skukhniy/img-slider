3534aa04e85187e996fb300ea879b8b6716868e1
Stan <skukhniy@gmail.com>
2022-01-07 20:49:43 -0500

    added five second loop

--- src/index.js
+++ src/index.js
@@ -20,34 +20,54 @@ function hide() {
   });
 }
 // grab the correct image, hide other images
-function render(index) {
+function render() {
   hide();
-  slides.item(index).classList.remove("hide");
-  dots.item(index).classList.remove("empty");
+  slides.item(globalIndex).classList.remove("hide");
+  dots.item(globalIndex).classList.remove("empty");
+}
+// increment global Index, reset to 0 if it passes 2.
+function increment() {
+  if (globalIndex >= 2) {
+    globalIndex = 0;
+  } else {
+    globalIndex += 1;
+  }
+}
+// function to loop every 5 seconds
+function intervalFunc() {
+  increment();
+  render();
+}
+// inital interval
+let fiveSeconds = setInterval(intervalFunc, 5000);
+
+// resets the interval, and then starts it again
+function resetInterval() {
+  clearInterval(fiveSeconds);
+  fiveSeconds = setInterval(intervalFunc, 5000);
 }
 
 // Add DOM func to each dot
 Array.from(dots).forEach((dot, index) => {
   dot.addEventListener("click", () => {
-    render(index);
+    globalIndex = index;
+    render();
+    resetInterval();
   });
 });
 
 // add function to the left and right arrow, influence global Index and render
 leftArrow.addEventListener("click", () => {
-  if (globalIndex === 0) {
+  if (globalIndex <= 0) {
     globalIndex = 2;
   } else {
     globalIndex -= 1;
   }
-  render(globalIndex);
+  render();
+  resetInterval();
 });
-
 rightArrow.addEventListener("click", () => {
-  if (globalIndex === 2) {
-    globalIndex = 0;
-  } else {
-    globalIndex += 1;
-  }
-  render(globalIndex);
+  increment();
+  render();
+  resetInterval();
 });
